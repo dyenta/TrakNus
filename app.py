db7a37e1f34a5c5db0046d70faa410ca7ce03781
dyenta <mochammad.dyenta@ui.ac.id>
2025-12-12 10:54:48 +0700

    update 12-12-2025

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ if menu == "Dashboard Analisa":
     
     # Ambil data agregat saja (agar ringan)
     # Contoh: Total sales per area
-    response = supabase.table("sales_data").select("Area, Amount-in-Local-Currency").execute()
+    response = supabase.table("sales_data").select("area, amount_in_local_currency").execute()
     df = pd.DataFrame(response.data)
     
     # Visualisasi
-    fig = px.bar(df.groupby("Area").sum().reset_index(), x="Area", y="Amount in Local Currency")
+    fig = px.bar(df.groupby("area").sum().reset_index(), x="Area", y="Amount in Local Currency")
     st.plotly_chart(fig, use_container_width=True)
 
 elif menu == "Upload Data Bulanan":
